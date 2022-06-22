e5ca821df8443291532299cb4e173eb74dc1e9d2
Braxton <65457602+braxttsl-dev@users.noreply.github.com>
2022-06-22 19:34:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ let currencyFormat = new Intl.NumberFormat('en-US', {
 
 
 // Slider functionality
+/* How this works -
+    The viewCount span is set to whatever value the range slider is at
+    The cost span is set to the viewCount times .16 and formmatted to get the amount in currency
+    Then if the checkbox is checked to yearly the cost span is then multiplied by .25*/
 rangeSlider.addEventListener("click", ()=>{
     viewCount.textContent = rangeSlider.value
     cost.textContent = currencyFormat.format(viewCount.textContent * .16)
@@ -25,6 +29,9 @@ rangeSlider.addEventListener("click", ()=>{
 })
 
 //Checkbox functionality
+/*How this works
+     This is just to make sure whenever the user switches from monthly to yearly 
+     that the price changes when the button is pressed not only when the range slider is pressed */
 billingSelect.addEventListener("click",()=>{
     if (billingSelect.checked == true){
         cost.textContent = currencyFormat.format(viewCount.textContent * .16 - viewCount.textContent * .16 * .25 )
